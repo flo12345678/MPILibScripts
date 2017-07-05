af82413340a0c7e7b2d462a099796af3bad7c16b
F.Griese <f.griese@uke.de>
2017-07-05 18:04:49 +0200

    selecte rectangle

--- src/UI/TestDrawing.jl
+++ src/UI/TestDrawing.jl
@@ -1,7 +1,7 @@
 using Gtk, GtkUtilities, Gtk.ShortNames, Cairo, MPILib, ImageMetadata
 
 import Base: getindex
-export TestDrawingWindow
+export TestDrawingWindow, emptyFunction, drawRectangle, calcMeta, createRectangle, getSliceSizes
 
 type TestDrawingWindow
   builder
@@ -23,29 +23,67 @@ function TestDrawingWindow()
 
   imSize = size(image)
   xy,xz,yz = getSliceSizes([50, 50, 25], [2,2,1])
-  drawAll(m.grid[1,1],image, imSize, xy, [0,0])
+  xyOffset = [0,0]
+  drawAll(m.grid[1,1],image, imSize, xy, xyOffset)
+  println("hallo")
+  m.grid[1,1].mouse.button3press = @guarded (widget, event) -> begin
+    ctx = getgc(m.grid[1,1])
+
+    reveal(m.grid[1,1])
+    println(event.x," ",event.y)
+    h = height(ctx)
+    w = width(ctx)
+    println("h ",h, "w", w)
+    p = [event.x,event.y]
+    #println("center", center(ctx))
+    println(typeof(ctx))
+    @guarded Gtk.draw(m.grid[1,1]) do widget
+      drawAll(m.grid[1,1],image, imSize, xy, xyOffset)
+      println("mouse event")
+      drawRectangle(ctx, h,w,p, imSize, xy, xyOffset, rgb=[0,0,1],lineWidth=3.0)
+    end
+    #m.grid[1,1].mouse.button3press=emptyFunction
+
+    # xx = event.x / w*size(image,2) + 0.5
+    # yy = event.y / h*size(image,1) + 0.5
+    #
+    # xx = !flipX ? xx : (size(image,2)-xx+1)
+    # yy = !flipY ? yy : (size(image,1)-yy+1)
+
+   end
 
   return w, m
+end
+
+function emptyFunction(widget::Gtk.GtkCanvas, event::Gtk.GdkEventButton)
 end
 
 function drawAll(control,image, imSize, xy, xyOffset)
   @guarded Gtk.draw(control) do widget
       ctx = getgc(control)
       copy!(ctx,image)
-      drawRectangle(ctx, imSize, xy, xyOffset)
+      h = height(ctx)
+      w = width(ctx)
+      p = [w/2,h/2] # changed order because of image coord system
+      println("redraw event")
+      drawRectangle(ctx, h,w,p, imSize, xy, xyOffset,lineWidth=5.0)
   end
 end
 
-function drawRectangle(ctx, imSize, xy, xyOffset)
-    h = height(ctx)
-    w = width(ctx)
-    cDA = [w/2,h/2] # changed order because of image coord system
-    cIA = [imSize[1]/2,imSize[2]/2]
-    sFac = cDA ./ cIA
-    set_source_rgb(ctx, 0, 1, 0)
-    createRectangle(ctx, cDA,cIA, sFac, xy, xyOffset)
-    set_line_width(ctx, 3.0)
-    Cairo.stroke(ctx)
+function drawRectangle(ctx,h,w, p, imSize, xy, xyOffset;rgb=[0,1,0], lineWidth=3.0)
+  cIA, sFac = calcMeta(p,h,w, imSize)
+  set_source_rgb(ctx, rgb...)
+  createRectangle(ctx, p, cIA, sFac, xy, xyOffset)
+  set_line_width(ctx, lineWidth)
+  Cairo.stroke(ctx)
+end
+
+function calcMeta(p,h,w,imSize)
+  cDA = [w/2,h/2]
+  cIA = [imSize[1]/2,imSize[2]/2]
+  sFac = cDA ./ cIA
+  println("cIA", cIA, "sFac", sFac)
+  return cIA, sFac
 end
 
 function createRectangle(ctx, cDA, cIA, sFac, xy, xyOffset)
